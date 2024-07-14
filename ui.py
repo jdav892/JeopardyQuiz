49a21e904a170251f7876f29a84a954c4bd55ebc
jdav892 <jadavila9@gmail.com>
2024-07-14 13:39:01 -0400

    background color

--- ui.py
+++ ui.py
@@ -8,7 +8,7 @@ class QuizInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler")
-        
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
         
         self.window.mainloop()
         
