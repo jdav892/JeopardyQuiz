5ab806986fb0c320227e91bae886caaa7a005ce9
jdav892 <jadavila9@gmail.com>
2024-07-14 14:02:24 -0400

    buttons added

--- ui.py
+++ ui.py
@@ -12,8 +12,22 @@ class QuizInterface:
         self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
         self.score_label.grid(row=0, column=1)
         self.canvas = Canvas(width=300, height=200, bg="white")
-        self.question_text = self.canvas.create_text(150, 125,text="Some Question Text", fill=THEME_COLOR)
-        self.canvas.grid(row=1, column=0, columnspan=2)
+        self.question_text = self.canvas.create_text(
+            150, 
+            125,
+            text="Some Question Text", 
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic")
+            )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+        
+        true_image = PhotoImage(file="C:/Users/jay-5/Documents/code/pythonProj/100daysOfPy/quizAPP/images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
+        
+        false_image = PhotoImage(file="C:/Users/jay-5/Documents/code/pythonProj/100daysOfPy/quizAPP/images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button.grid(row=2, column=1)
         
         
         
